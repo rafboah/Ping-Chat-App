e24e752112f511ed65d8156bc3c1f8f66600b60d
rafboah <rafboah@gmail.com>
2024-03-01 17:02:28 -0500

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from 'path'
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -10,6 +11,8 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/Socket.js";
 
 const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve()
 
 dotenv.config();
 
@@ -21,10 +24,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-// app.get("/", (req, res) => {
-//     //root route http://localhost:5000/
-//     res.send("Hello World!");
-// });
+app.use(express.static(path.join(__dirname, "/frontend/dist")))
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"))
+})
 
 server.listen(PORT, () => {
     connectToMongoDB();
