24b0471da2008ce1152aa20e96f77461fcbf3c4d
rafboah <rafboah@gmail.com>
2024-04-28 13:05:45 -0400

    Set the display height of sidebar to full

--- frontend/src/components/sidebar/Sidebar.jsx
+++ frontend/src/components/sidebar/Sidebar.jsx
@@ -4,7 +4,7 @@ import LogoutButton from './LogoutButton'
 
 const Sidebar = () => {
   return (
-    <div className='border-r border-gray-900 p-4 flex flex-col'>
+    <div className='h-full border-r border-gray-900 p-4 flex flex-col'>
         <SearchInput/>
         <div className='divider px-3'></div>
         <Chats/>
