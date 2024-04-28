58174336da3935ecd44d1e8433f20eef30271a36
rafboah <rafboah@gmail.com>
2024-04-28 12:28:59 -0400

    Changed display size

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,10 +1,9 @@
-import React from 'react'
 import Sidebar from '../../components/sidebar/Sidebar'
 import MessageContainer from '../../components/messages/MessageContainer'
 
 const Home = () => {
   return (
-    <div className='flex sm:h[450px] md:h[550px] rounded-lg overflow-hidden bg-clip-padding backdrop-filter bg-gray-800 backdrop-blur-lg'>
+    <div className='flex sm:h[100vh] md:h[100vh] rounded-lg overflow-hidden bg-clip-padding backdrop-filter bg-gray-800 backdrop-blur-lg'>
         <Sidebar/>
         <MessageContainer/>
     </div>
